03f51e8fdfce64f16a7997ac13e3e3f7c3e5e8e7
Arkadiusz Chudy <arkadiusz.chudy@bd.com>
2022-02-13 20:54:42 +0000

    Add Document entity + migrations

---
+++ AzureMVC.Infrastructure/Migrations/20220213193850_Document.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AzureMVC.Infrastructure.Migrations
+{
+    public partial class Document : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Document",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Document", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Document");
+        }
+    }
+}
